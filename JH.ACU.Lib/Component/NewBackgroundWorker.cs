cef830539f7c7434eb478021d0bc134ada3039b5
gilbert_hbd <gilbert_hbd@qq.com>
2017-04-12 11:26:53 +0800

    补充丢失文件

---
+++ JH.ACU.Lib/Component/NewBackgroundWorker.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+
+namespace JH.ACU.Lib.Component
+{
+    public partial class NewBackgroundWorker : BackgroundWorker
+    {
+        public NewBackgroundWorker()
+        {
+            InitializeComponent();
+            WorkerReportsProgress = true;
+            WorkerSupportsCancellation = true;
+        }
+
+        public NewBackgroundWorker(IContainer container)
+        {
+            container.Add(this);
+
+            InitializeComponent();
+            WorkerReportsProgress = true;
+            WorkerSupportsCancellation = true;
+
+        }
+    }
+}
